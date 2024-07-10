8463d020c3bfeb97e9a95f56d9ec44cf9551a393
Adrieto <parodiadrian3@gmail.com>
2024-07-10 11:22:48 -0300

    Primeros elementos html (header) pegados en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import bumeran from '/img/Imagen-navbar.png'
+// import bumeran from '/vite.svg'
 import './App.css'
 
 function App() {
 
 
   return (
-    "Hola mundo, Soy el melhor"
+    <header>
+        <div class="header-container">
+            <div class="header-izq">
+                <p>Portafolio</p>
+                <img src={bumeran} alt=""/>
+            </div>
+            <nav>
+                <a href="#hero">Principal</a>
+                <a href="#sobre-mi">Sobre</a>
+                <a href="#habilidades">Habilidades</a>
+                <a href="#proyectos">Proyectos</a>
+                <a href="#servicios">Servicios</a>
+                <a href="#testimonios">Testimonios</a>
+                <a href="#contacto">Contacto</a>
+                <i class="bi bi-list"></i>
+            </nav>
+            <div class="div-boton">
+                <a href="#contacto">
+                    <button class="small-button">Contratarme</button>
+                </a>
+                
+            </div>
+        </div>
+      </header>
   )
 }
 
