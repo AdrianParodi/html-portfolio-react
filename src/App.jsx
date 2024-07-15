d400a48f12f95b4bd60cfa8965dd1d79157085f8
Adrieto <parodiadrian3@gmail.com>
2024-07-15 12:38:04 -0300

    Componente SobreMi agregado junto a su Css

--- src/App.jsx
+++ src/App.jsx
@@ -28,48 +28,6 @@ function App() {
   return (
     <>
             
-      {/* <!-- Seccion Sobre mi --> */}
-      <section className="sobre-mi" id="sobre-mi">
-        {/* <!-- Subseccion con la foto y el cuadro de borde blanco --> */}
-        <h1>Sobre mi</h1>
-        <section className="sobre-mi-top">
-            <img className="foto-sobre-mi" src={fotoSobreMi} alt="fotografía del desarrollador"></img>
-            <div className= "div-borde-blanco">
-                <h2>Diseñar no es un problema para mí. Soy diseñador web y programador en Python</h2>
-                <p> Actualmente brindo mis servicios en <em>Upwork.com</em> en el area de ingeniería y programación, principalmente en Matlab y en Python. Asimismo, trabajo en el desarrollo del backend para aplicaciones en Android y en el desarrollo web. <br></br>
-                Adicionalmente, brindo clases de química en idioma inglés desde hace mas de 5 años en la plataforma <em>Tutor.com</em></p>
-                <a href="#servicios">
-                    <button className="services-button">
-                    <i className="fa-solid fa-arrow-right-long flechas-icons"></i>
-                    Consultar Servicios Disponibles
-                    <i className="fa-solid fa-arrow-left-long flechas-icons"></i>
-                    </button>
-                </a>
-            </div>
-        </section>
-
-        {/* <!-- Subseccion de los cuadros con borde amarillo e imagenes --> */}
-        <section className="destacados" id="destacados">
-            <div>
-              <FontAwesomeIcon icon={faDesktop}/>
-              <h2>Diseño Web</h2>
-              <p>Foco en el diseño responsive de sitios web y sus funcionalidad con JS. </p>
-            </div>
-
-            <div>
-              <FontAwesomeIcon icon={faMobileScreenButton} />    
-              <h2>Backend de aplicaciones</h2>
-              <p>Desarrollo de funcionalidades para 2 aplicaciones disponibles en la app store de google.</p>
-            </div>
-
-            <div>
-                <FontAwesomeIcon icon={faChartLine}/>
-                <h2>+ 90% de éxito</h2>
-                <p>En proyectos desarrollados como freelancer.</p>
-            </div>
-        </section>
-      </section>
-
         {/* <!-- Seccion Habilidades --> */}
       <section id="habilidades">
         <h1>Habilidades</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import './index.css'
 import Header from './Component/Header/Header.jsx'
 import Footer from './Component/Footer/Footer.jsx'
 import Hero from './Component/Hero/Hero.jsx'
+import SobreMi from './Component/SobreMi/SobreMi.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
     <Hero/>
+    <SobreMi/>
     <App/>
     <Footer/>
   </React.StrictMode>,
