d1d054fb7ef6f496e3f32b3a81456c13a3830b3e
Adrieto <parodiadrian3@gmail.com>
2024-07-21 23:04:54 -0300

    Funcionalidad de guardar los campos ingresados en el formulario desarrollada. Falta afinar el comportamiento del formulario al tocar el boton de envio

--- src/Component/Contacto/Contacto.jsx
+++ src/Component/Contacto/Contacto.jsx
@@ -10,6 +10,10 @@ const Contacto=()=>{
 
   function manejarEnvio(e){
     e.preventDefault()
+    console.log(nombre)
+    console.log(apellido)
+    console.log(email)
+    console.log(mensaje)
 
   }
 
@@ -23,15 +27,15 @@ const Contacto=()=>{
           <input type="text" id="fname" name="fname" placeholder="Nombre *" value={nombre} onChange={(e)=>setNombre(e.target.value)}></input>
         </div>
         <div>
-          <input type="text" id="lname" name="lname" placeholder="Apellido *"></input>
+          <input type="text" id="lname" name="lname" placeholder="Apellido *" value={apellido} onChange={(e)=>setApellido(e.target.value)}></input>
         </div>
         
         <div>
-          <input type="email" id="email" name="email" placeholder="Email *"></input>
+          <input type="email" id="email" name="email" placeholder="Email *" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
         </div>
 
         <div>
-          <textarea id="mensaje" name="mensaje" required placeholder="Mensaje *"></textarea>
+          <textarea id="mensaje" name="mensaje" required placeholder="Mensaje *" value={mensaje} onChange={(e)=>setMensaje(e.target.value)} ></textarea>
         </div>
 
         <div>
