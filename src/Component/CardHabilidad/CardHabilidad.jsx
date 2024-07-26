d8f2970550852ccda886a610b5cbcd20f58b44f5
Adrieto <parodiadrian3@gmail.com>
2024-07-26 11:32:27 -0300

    Tamaño 'responsive' de iconos en todas las secciones ajustados

--- src/Component/CardHabilidad/CardHabilidad.jsx
+++ src/Component/CardHabilidad/CardHabilidad.jsx
@@ -1,14 +1,14 @@
 // import './CardHabilidad.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {colors} from '../../themes/themes.js'
-import {Flex, Text} from '@chakra-ui/react'
+import {Flex, Text, Box} from '@chakra-ui/react'
 
 
 const CardHabilidad = ({titulo, icono, descripcion})=>{
 
 return(
     <Flex className='tarjeta-habilidades' flexDirection='column' borderStyle='solid' borderColor={colors.color3} borderWidth='1px' borderRadius='20px' bg={colors.color3} color={colors.color1} alignItems='center' textAlign='center' p='20px' my='40px' width='30%'>
-        <FontAwesomeIcon  className='icono-habilidad' icon={icono} size='5x'/>
+        <Box as ={FontAwesomeIcon} icon={icono} fontSize={['2.5rem', '3rem', '4rem', '5rem']}/>
         <Text as='h2' color={colors.color1} my='25px'>{titulo}</Text>
         <Text as='p' color={colors.color1} lineHeight='25px'>{descripcion} </Text>
     </Flex>
--- src/Component/CardServicio/CardServicio.jsx
+++ src/Component/CardServicio/CardServicio.jsx
@@ -1,6 +1,6 @@
 // import './CardServicio.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {Text, Flex, Button} from '@chakra-ui/react'
+import {Text, Flex, Box, Button} from '@chakra-ui/react'
 import {colors} from '../../themes/themes.js'
 
 
@@ -9,7 +9,7 @@ const CardServicio=({titulo, icono, descripcion})=>{
     const fW1 = 600 //fontWeight
     return(
         <Flex className='tarjeta-servicios' flexDirection='column'  width='30%' borderStyle='dashed' borderColor={colors.color3} borderWidth='1px' borderRadius='20px' my='20px' p='20px'>
-            <FontAwesomeIcon className='icono-servicio'  icon={icono} color={colors.color3} size='5x'/>
+            <Box as ={FontAwesomeIcon}   icon={icono} color={colors.color3} fontSize={['2.5rem', '3rem', '4rem', '5rem']}/>
             <Flex className="textoMasBoton" flexDirection='column' alignItems='center' textAlign='center' flex='1'>
                 <Text as='h2' color={colors.color2} my='25px'>{titulo}</Text>
                 <Text as='p' color={colors.color2} my='15px' lineHeight='25px'>{descripcion} </Text>
